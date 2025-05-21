60d02383b6c08133733d9fe50143791ef1b487cb
Mohamed Algharib <mohamedalgharib26@gmail.com>
2025-05-21 20:33:22 +0300

    adding layout

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -26,7 +26,7 @@ function ProductList() {
         <div className="container px-4 px-lg-5 mt-5">
           <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             {Products.map((el) => (
-              <Product key={el.id} />
+              <Product key={el.id} product={el} />
             ))}
           </div>
         </div>
